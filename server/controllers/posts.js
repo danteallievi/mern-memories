79ebc489126f8f154f6f4763ebcacfdf4034e061
Dante Allievi <toto99.-@hotmail.com>
2021-05-18 15:06:47 +0200

    Update post id problem fixed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -75,12 +75,12 @@ export const updatePost = async (req, res) => {
   const { id } = req.params;
   const post = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send('No post with that id');
 
   const updatedPost = await PostMessage.findByIdAndUpdate(
-    _id,
-    { ...post, _id },
+    id,
+    { ...post, id },
     { new: true }
   );
 
